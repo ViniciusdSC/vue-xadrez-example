0892bd2668bb88d1df5717a1438134bc00ed5b77
Vinicius Cardoso <viniciuscardoso@localhost.localdomain>
2020-03-01 00:12:24 -0300

    init

---
+++ src/components/Field.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div v-for="fields in matrizField" class="field">
+    <div v-for="field in fields" class="">
+      <component v-bind:is="field" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    matrizField: []
+  }),
+  methods: {
+    initialize () {
+      matrizField = []
+      for (let i; i < 8; i++) {
+        matrizField[i] = []
+        for (let j; j < 8;  j++) {
+          matrizField[i][j] = ''
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/Pieces/Index.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  methods: {
+    up () {
+
+    },
+    left () {
+
+    },
+    right () {
+
+    },
+    down () {
+      
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
